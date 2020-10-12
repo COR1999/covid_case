d2e463b0417ed4b5f1ee55cf1d2cb45c0f8db18b
COR1999 <cian.orourke@gmail.com>
2020-10-12 22:08:18 +0100

    moved static files into staticfiles dir

--- covid_case/settings.py
+++ covid_case/settings.py
@@ -190,7 +190,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "media")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
