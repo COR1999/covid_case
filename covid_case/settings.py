e1c80c920648ce4ac2e2805f7380593c0298e449
COR1999 <cian.orourke@gmail.com>
2020-10-13 09:57:15 +0100

    moved django_heroku.settings(locals()) to bottom of settings file

--- covid_case/settings.py
+++ covid_case/settings.py
@@ -215,10 +215,6 @@ STRIPE_CURRENCY = "USD"
 CONST_PROFILE_ATTR = 200
 CONST_PROFILE_ADDRESS = 1000
 
-django_heroku.settings(locals())
-
-
-
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = 'covid-case-bucket'
@@ -226,3 +222,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
+
+
